0831e28255ef060ebae075a91158fb6a907d51ad
Michel KACOU <Your email>
2023-03-14 17:28:10 +0100

    first commit

---
+++ src/Sidebar.js
@@ -0,0 +1,49 @@
+import { Avatar } from "@material-ui/core";
+import React from "react";
+import "./Sidebar.css";
+
+function Sidebar() {
+    const recentItem = (topic) => (
+        <div className="sidebar__recentItem">
+            <span className="sidebar__hash">#</span>
+            <p>{topic}</p>
+        </div>
+    )
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <img
+          src="https://images.unsplash.com/photo-1558376939-7d6cb3025d5c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGNvdmVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+        <Avatar className="sidebar__avatar" />
+        <h2>Michel KACOU</h2>
+        <h4>michelkacou10@gmail.com</h4>
+      </div>
+
+      <div className="sidebar__stats">
+        <div className="sidebar__stat">
+          <p> Who viewed you?</p>
+          <p className="sidebar__statNumber">2,543</p>
+        </div>
+
+        <div className="sidebar__stat">
+          <p>View on post</p>
+          <p className="sidebar__statNumber">2,448</p>
+        </div>
+      </div>
+
+      <div className="sidebar__bottom">
+        <p>Recent</p>
+        {recentItem('reactjs')}
+        {recentItem('programming')}
+        {recentItem('softwareengeneering')}
+        {recentItem('design')}
+        {recentItem('developer')}
+
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
